65e048651ffa89a1997bb670ad0cc5c05dbd93d5
MDevoldere <mdevoldere@users.noreply.github.com>
2022-04-19 15:51:06 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
+// importer le module express
 const express = require('express');
 
 // application express
 const app = express();
 
+// middleware : s'exécute à chaque requête
 app.use((req, res, next) => {
     let method = req.method;
     let path = req.originalUrl;
@@ -10,13 +12,15 @@ app.use((req, res, next) => {
     next()
 })
 
+// gestion des fichiers statiques
 app.use('/public', express.static(__dirname + '/public'))
 
+// import des routes (voir le fichier routes/index.js)
 const router = require('./routes')
-
+// association du router à l'app
 app.use('/', router)
 
-
+// démarrage du serveur
 app.listen(80, () => {
     console.log("Serveur prêt (http://localhost)")
 });
